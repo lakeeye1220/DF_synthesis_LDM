17aae5fb60ee6d2c81f5c448c74739d789a6173b
yujinKim <lakeeye1220@gmail.com>
2024-04-11 16:14:32 +0000

    domain token embedding optimization

---
+++ imnet_r_classes.py
@@ -0,0 +1,10 @@
+IDX2NAME = {
+    0: "stingray",#n01498041
+    1: "junco", #n01534433
+    2: "grey_whale", #n02066245
+    3: "assault_rifle", #n02749479
+    4: "hammer",#n03481172
+    5:"hatchet",#n03498962
+    6:"lipstick",#n03676483
+    7:"military_aircraft"#n04552348
+}
